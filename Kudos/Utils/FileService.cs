c8990a6c15eed770f5ce1d320d3a1bbc6f13e6ed
Maximilian Mayrhofer <42514976+TheBlue-1@users.noreply.github.com>
2020-06-10 18:27:51 +0200

    fix (#3)

--- Kudos/Utils/FileService.cs
+++ Kudos/Utils/FileService.cs
@@ -24,64 +24,70 @@ namespace Kudos.Utils {
 		}
 
 		private object ByteArrayToObject(byte[] arr) {
-			using (MemoryStream memoryStream = new MemoryStream()) {
-				BinaryFormatter binaryFormatter = new BinaryFormatter();
-				try {
-					memoryStream.Write(arr, 0, arr.Length);
-					memoryStream.Seek(0, SeekOrigin.Begin);
-					object obj = binaryFormatter.Deserialize(memoryStream);
-					return obj;
-				}
-				catch (Exception) {
-					return null;
-				}
+			using MemoryStream memoryStream = new MemoryStream();
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			try {
+				memoryStream.Write(arr, 0, arr.Length);
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				object obj = binaryFormatter.Deserialize(memoryStream);
+				return obj;
+			}
+			catch (Exception) {
+				return null;
 			}
 		}
 
 		private byte[] ObjectToByteArray(object obj) {
 			BinaryFormatter binaryFormatter = new BinaryFormatter();
-			using (MemoryStream memoryStream = new MemoryStream()) {
-				binaryFormatter.Serialize(memoryStream, obj);
-				return memoryStream.ToArray();
-			}
+			using MemoryStream memoryStream = new MemoryStream();
+			binaryFormatter.Serialize(memoryStream, obj);
+			return memoryStream.ToArray();
 		}
 
 		public async Task<T> ReadFromFile<T>(string fileName)
 			where T : new() {
-			fileName = Path.Combine(ApplicationFolderPath, fileName);
-			if (File.Exists(fileName)) {
-				byte[] byteContent = File.ReadAllBytes(fileName);
-				T val = (T)ByteArrayToObject(byteContent);
-				if (val != null) {
-					return val;
+			return await Task.Run(() => {
+				fileName = Path.Combine(ApplicationFolderPath, fileName);
+				if (File.Exists(fileName)) {
+					byte[] byteContent = File.ReadAllBytes(fileName);
+					T val = (T)ByteArrayToObject(byteContent);
+					if (val != null) {
+						return val;
+					}
 				}
-			}
-			return new T();
+				return new T();
+			});
 		}
 
 		public async Task<T> ReadJsonFromFile<T>(string fileName)
 			where T : new() {
-			fileName = Path.Combine(ApplicationFolderPath, fileName);
-			if (File.Exists(fileName)) {
-				string json = File.ReadAllText(fileName);
-				T val = JsonConvert.DeserializeObject<T>(json);
-				if (val != null) {
-					return val;
+			return await Task.Run(() => {
+				fileName = Path.Combine(ApplicationFolderPath, fileName);
+				if (File.Exists(fileName)) {
+					string json = File.ReadAllText(fileName);
+					T val = JsonConvert.DeserializeObject<T>(json);
+					if (val != null) {
+						return val;
+					}
 				}
-			}
-			return new T();
+				return new T();
+			});
 		}
 
 		public async Task SaveJsonToFile<T>(string fileName, T content) {
-			fileName = Path.Combine(ApplicationFolderPath, fileName);
-			string json = JsonConvert.SerializeObject(content);
-			File.WriteAllText(fileName, json);
+			await Task.Run(() => {
+				fileName = Path.Combine(ApplicationFolderPath, fileName);
+				string json = JsonConvert.SerializeObject(content);
+				File.WriteAllText(fileName, json);
+			});
 		}
 
 		public async Task SaveToFile<T>(string fileName, T content) {
-			fileName = Path.Combine(ApplicationFolderPath, fileName);
-			byte[] byteContent = ObjectToByteArray(content);
-			File.WriteAllBytes(fileName, byteContent);
+			await Task.Run(() => {
+				fileName = Path.Combine(ApplicationFolderPath, fileName);
+				byte[] byteContent = ObjectToByteArray(content);
+				File.WriteAllBytes(fileName, byteContent);
+			});
 		}
 	}
 }
