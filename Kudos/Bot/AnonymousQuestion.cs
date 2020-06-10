8826a88dcfaa3e8b92a2e9fc587a6c3464e7cadd
Maximilian Mayrhofer <42514976+TheBlue-1@users.noreply.github.com>
2020-06-10 23:47:12 +0200

    add messages (#4)
    * add messages
    
    * same workflow as master

---
+++ Kudos/Bot/AnonymousQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kudos.Bot
+{
+    public class AnonymousQuestion
+    {
+        /// <summary>
+        /// TODO user sends bot a question private, bot sends it to receiver, bot sends answer back to questionnaire
+		/// </summary>
+
+		public static AnonymousQuestion Instance { get; } = new AnonymousQuestion();
+
+		static AnonymousQuestion() { }
+
+		private AnonymousQuestion() { }
+
+    }
+}
---
+++ Kudos/Extensions/SocketUserExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.WebSocket;
+
+namespace Kudos.Extensions
+{
+  public static class SocketUserExtension
+    {
+	public	static string Mention(this SocketUser user) => $"<@ {user.Id}>";
+	}
+}
