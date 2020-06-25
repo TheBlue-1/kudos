facea1482c23ac9c33a800c594e7e981c16298ec
TheBlue-1 <muajunkacc@gmail.com>
2020-06-25 14:46:27 +0200

    begin botlist api use

---
+++ Kudos/Utils/BotList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordBotsList.Api;
+using DiscordBotsList.Api.Objects;
+
+namespace Kudos.Utils
+{
+   public class BotList {
+	   private AuthDiscordBotListApi Api { get; set; }
+	   public IDblSelfBot ThisBot { get; private set; }
+
+		private  BotList() {
+
+		}
+
+
+
+		public static async Task<BotList> Instantiate(ulong botId,string topGgToken) {
+			BotList botList = new BotList { Api = new AuthDiscordBotListApi(botId, topGgToken) };
+			botList.ThisBot = await botList.Api.GetMeAsync();
+			return botList;
+		}
+
+	}
+}
