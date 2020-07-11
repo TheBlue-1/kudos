b980eb94e2ab274bdb2eb7a66e6183d77e7ec3b5
TheBlue-1 <muajunkacc@gmail.com>
2020-07-11 22:32:26 +0200

    changes

--- Kudos/Bot/Modules/Stalker.cs
+++ Kudos/Bot/Modules/Stalker.cs
@@ -1,12 +1,13 @@
 ﻿#region
 using System;
-using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Discord.Audio;
 using Discord.WebSocket;
 using Kudos.Attributes;
 using Kudos.Exceptions;
 using Kudos.Extensions;
+using NAudio.Wave;
 #endregion
 
 namespace Kudos.Bot.Modules {
@@ -19,32 +20,35 @@ namespace Kudos.Bot.Modules {
 		private Stalker() { }
 
 		[Command("stalk")]
-		public async Task Stalk([CommandParameter]SocketUser author,[CommandParameter(0)] SocketUser victim) {
+		public async Task Stalk([CommandParameter] SocketUser author, [CommandParameter(0)] SocketUser victim) {
 			ISocketAudioChannel channel = victim.AudioChannel();
 			if (channel == null) {
 				throw new KudosArgumentException("user not in voice channel");
 			}
 			IAudioClient client = await channel.ConnectAsync();
-			
-			client.StreamCreated += async (id, stream) => {
-				byte[] buffer = new byte[1024* 8];
-				FileStream file = File.OpenWrite("D:\\Downloads\\test.audio");
-				while (true) {
-					int read = await stream.ReadAsync(buffer, 0, 1024 * 8);
-					await file.WriteAsync(buffer, 0, read);
-				}
-			};
-		var	stream = client.CreateOpusStream();
-		byte[] buffer = new byte[1024 * 1];
-		FileStream file = File.OpenRead("D:\\Downloads\\test.opus");
-		int last = 1024 * 1;
-		long r = file.Length;
-		while (last== 1024 * 1 && r>0)
-		{
-			last=	await file.ReadAsync(buffer, 0, (int)Math.Min(1024 * 1,r));
-			r -= last;
-			await stream.WriteAsync(buffer, 0, last);
-		}
+
+				client.StreamCreated += async (id, stream) => {
+					const int bitrate = 92000;
+					WaveFormat format=new WaveFormat(bitrate/16,16,1);
+					await using CueWaveFileWriter writer = new CueWaveFileWriter("D:\\Downloads\\out.wav", format);
+					CancellationTokenSource source=new CancellationTokenSource(30000);
+					try { 
+					await stream.CopyToAsync(writer,source.Token);
+					}
+					catch (Exception e) {
+					
+					}
+					// ReSharper disable once MethodSupportsCancellation
+					await writer.FlushAsync();
+				};
+					
+
+			await using Mp3FileReader fileReader = new Mp3FileReader("D:\\Downloads\\test.mp3");
+			{
+				int bitrate = 92000;
+				AudioOutStream dstream = client.CreatePCMStream(AudioApplication.Mixed, bitrate);
+				await fileReader.CopyToAsync(dstream);
+			}
 		}
 	}
 }
