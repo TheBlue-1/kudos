81261b6d34d6293b584836217bbc6d63645c0584
Maximilian Mayrhofer <42514976+TheBlue-1@users.noreply.github.com>
2020-10-27 15:16:22 +0100

    implement database (#39)

---
+++ Kudos/DatabaseModels/KudosDataContext.cs
@@ -0,0 +1,31 @@
+﻿#region
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+#endregion
+
+namespace Kudos.DatabaseModels {
+	[SuppressMessage("ReSharper", "UnusedMember.Global")]
+	[SuppressMessage("ReSharper", "PartialTypeWithSinglePart")]
+	public partial class KudosDataContext : DbContext {
+		public DbSet<HonorData> HonorData { get; set; }
+		public DbSet<QuestionData> QuestionData { get; set; }
+
+		public KudosDataContext() { }
+
+		public KudosDataContext(DbContextOptions<KudosDataContext> options) : base(options) { }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			if (!optionsBuilder.IsConfigured) {
+				optionsBuilder.UseSqlite("DataSource=KudosData.db");
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			// ReSharper disable once InvocationIsSkipped
+			OnModelCreatingPartial(modelBuilder);
+		}
+
+		// ReSharper disable once PartialMethodWithSinglePart
+		partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+	}
+}
---
+++ Kudos/Migrations/KudosDataContextModelSnapshot.cs
@@ -0,0 +1,48 @@
+﻿#region
+using System;
+using Kudos.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+#endregion
+
+namespace Kudos.Migrations {
+	[DbContext(typeof (KudosDataContext))]
+	// ReSharper disable once UnusedMember.Global
+	internal class KudosDataContextModelSnapshot : ModelSnapshot {
+		protected override void BuildModel(ModelBuilder modelBuilder) {
+		#pragma warning disable 612, 618
+			modelBuilder.HasAnnotation("ProductVersion", "3.1.9");
+
+			modelBuilder.Entity("Kudos.DatabaseModels.HonorData", b => {
+				b.Property<ulong>("Id").ValueGeneratedOnAdd().HasColumnType("INTEGER");
+
+				b.Property<int>("Honor").HasColumnType("INTEGER");
+
+				b.Property<ulong>("Honored").HasColumnType("INTEGER");
+
+				b.Property<ulong>("Honorer").HasColumnType("INTEGER");
+
+				b.Property<DateTime>("Timestamp").HasColumnType("TEXT");
+
+				b.HasKey("Id");
+
+				b.ToTable("HonorData");
+			});
+
+			modelBuilder.Entity("Kudos.DatabaseModels.QuestionData", b => {
+				b.Property<ulong>("Id").HasColumnType("INTEGER");
+
+				b.Property<ulong>("Answerer").HasColumnType("INTEGER");
+
+				b.Property<string>("Question").HasColumnType("TEXT");
+
+				b.Property<ulong>("Questionnaire").HasColumnType("INTEGER");
+
+				b.HasKey("Id");
+
+				b.ToTable("QuestionData");
+			});
+		#pragma warning restore 612, 618
+		}
+	}
+}
