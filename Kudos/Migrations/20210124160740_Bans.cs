0fb685d49bff52c7fb1c32677a6c2cb8fe5be701
TheBlue-1 <muajunkacc@gmail.com>
2021-01-24 17:08:12 +0100

    db update

---
+++ Kudos/Migrations/20210124160740_Bans.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kudos.Migrations
+{
+    public partial class Bans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BanData",
+                columns: table => new
+                {
+                    UserId = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HardBan = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BanData", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BanData");
+        }
+    }
+}
