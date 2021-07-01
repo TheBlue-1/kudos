785b2c049a6c479d72c98870cc7bab57dba10da8
TheBlue-1 <muajunkacc@gmail.com>
2021-07-01 13:40:38 +0200

    update updatfile

--- update.bash
+++ update.bash
@@ -1,14 +1,12 @@
-#stop old version
-cd Kudos/bin/Release/net5.0/linux-x64/publish
-pid=$(cat 'last.pid')
-kill $pid
 #pull
-cd ../../../../../../..
 git pull
 #build
 dotnet restore
 dotnet publish --configuration Release --framework net5.0 -p:PublishSingleFile=false -p:PublishTrimmed=false --runtime linux-x64 --self-contained true
-#run new version
+#stop old version
 cd Kudos/bin/Release/net5.0/linux-x64/publish
+pid=$(cat 'last.pid')
+kill $pid
+#run new version
 nohup ./Kudos &>> runningLog.txt &
 echo "$!" > last.pid
