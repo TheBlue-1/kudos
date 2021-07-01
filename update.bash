1e8c73bc532acafde0de8370c22731f041558600
TheBlue-1 <muajunkacc@gmail.com>
2021-07-01 13:58:05 +0200

    update update.bash

--- update.bash
+++ update.bash
@@ -7,7 +7,7 @@ cd Kudos/bin/Release/net5.0/linux-x64/publish
 pid=$(cat 'last.pid')
 kill $pid
 #build
-cd ../../../../../../..
+cd ../../../../../..
 dotnet restore
 dotnet publish --configuration Release --framework net5.0 -p:PublishSingleFile=false -p:PublishTrimmed=false --runtime linux-x64 --self-contained true
 #run new version
