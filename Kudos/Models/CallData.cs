7b196098f438b750c5227b4ab002c2b4d9c2d4a9
TheBlue-1 <muajunkacc@gmail.com>
2021-11-18 09:41:35 +0100

    add call lasted message, clean up

---
+++ Kudos/Models/CallData.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+
+namespace Kudos.Models {
+
+    public class CallData {
+        public IList<IUserMessage> CurrentInvites { get; set; }
+        public DateTime Timeout { get; set; }
+
+        public DateTime Start { get; } = DateTime.Now;
+
+        public IUser StartedBy { get; }
+
+        public CallData(IUser startedBy) {
+            StartedBy = startedBy;
+        }
+    }
+}
